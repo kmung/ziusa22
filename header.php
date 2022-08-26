4bae3dfd76c17b32625e2d18131c9ce1fea1e63a
Khawm Mung <johnmungtawng5@gmail.com>
2022-08-26 15:56:40 -0500

    favicon

--- header.php
+++ header.php
@@ -8,6 +8,13 @@
     <!--Site Title-->
     <title>Zomi Innkuan USA | Zomi Community of USA</title>
     <!--Site Favicon-->
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/favicon/site.webmanifest">
+    <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
     <meta name="description" content="The official website of the Zomi Community USA, Inc. Also known as Zomi Innkuan USA or ZIUSA." />
     <!--Stylesheets-->
     <link rel="stylesheet" href="/css/index.css">
