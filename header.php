b4c4f58f29c48ecbe4d69811bc0b22a7c3d7a431
Khawm Mung <johnmungtawng5@gmail.com>
2022-08-26 15:46:54 -0500

    Update header.php

--- header.php
+++ header.php
@@ -5,7 +5,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <!--Site Title-->
+    <title>Zomi Innkuan USA | Zomi Community of USA</title>
+    <!--Site Favicon-->
+    <meta name="description" content="The official website of the Zomi Community USA, Inc. Also known as Zomi Innkuan USA or ZIUSA." />
+    <!--Stylesheets-->
+    <link rel="stylesheet" href="/css/index.css">
 </head>
 <body>
     
